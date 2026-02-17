b27a66e3cbb0baf991e31db86853a35b5ef36476
TadekT <tadeusz.toporkiewicz@gmail.com>
2026-02-17 22:48:32 +0100

    problemy natury logicznej

---
+++ Assets/Script/AI_Enemy/EnemyAI_Brain.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class EnemyAI_Brain : MonoBehaviour
+{
+private enum State
+{
+    Idle, Patrol, Search, Chase, Attack
+}
+
+private State currentState;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+}
