07dc76c7b9fe2c8e11e6291da5a79b14770b6e26
Tadeusz <tadeusz.toporkiewicz@gmail.com>
2026-05-24 00:10:16 +0200

    Dodanie enemy3 posiadającego broń

--- Assets/Script/AI_Enemy/EnemyAI_Combat.cs
+++ Assets/Script/AI_Enemy/EnemyAI_Combat.cs
@@ -5,6 +5,10 @@ public class EnemyAI_Combat : MonoBehaviour
 
     [SerializeField] private OnTriggerEnterCombatDetection combatTrigger;
     [SerializeField] private int damage = 10;
+
+    [SerializeField] private Transform bulletSpawn;
+    [SerializeField] private GameObject bulletPrefab;
+
     private IDamageable currentTarget;
 
     public bool IsPlayerInAttackRange => currentTarget!= null;
