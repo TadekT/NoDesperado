3de477765b3f550efa8ba667c1b2db875df68abd
Tadeusz <tadeusz.toporkiewicz@gmail.com>
2026-05-25 11:19:22 +0200

    Wstępny pool dla obiektów bulletPrefab

---
+++ Assets/Script/AI_Enemy/Enemy3/BulletPool.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private int poolSize = 5;
+
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+
+
+    private void Awake()
+    {   
+        
+        for(int i = 0;i < poolSize; i++)
+        {
+            
+            GameObject bullet = Instantiate(bulletPrefab,transform);
+            bullet.SetActive(false);
+            pool.Enqueue(bullet);
+
+        }
+
+    }
+
+    public GameObject Get()
+    {
+
+        if(pool.Count > 0)
+        {
+            GameObject bullet = pool.Dequeue();
+            bullet.SetActive(true);
+            return bullet;            
+        }
+
+        return Instantiate(bulletPrefab,transform);
+    }
+
+
+    public void Return(GameObject bullet)
+    {
+        bullet.SetActive(false);
+        pool.Enqueue(bullet);
+    }
+
+
+}
+
